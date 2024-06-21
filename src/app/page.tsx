f5cbeefb5ee2d0d8e79c18a81fc3e3c512fb6dbf
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-06-21 10:32:43 +0200

    Update page layout, images, logo, and page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import Hero from '@/components/Hero';
 export default function Home() {
   return (
   <main className="flex flex-col items-center justify-between pt-24 px-12 m:px-28 ">
-	<div style={{ minWidth: '300px', minHeight: '106px' }}>
+	{/* <div style={{ minWidth: '300px', minHeight: '106px' }}>
 		<Image src="/media/images/logo-16-9.png" alt="LM-Art" width={800} height={200}  />
 	</div>
 	<Image src="/media/images/banner_1.jpg" alt="LM-Art" width={800} height={200}  />
-	<p className="py-5 text-l ">Art is the cure for the soul.</p>
-	<div className="pt-16 m:pt-24 pb-6"></div>
-	<div className="flex gap-4 pb-4">
+	<p className="py-5 text-sm ">Art is the cure for the soul.</p>
+	<div className="pt-16 m:pt-24 pb-6"></div> */}
+	<div className="flex gap-4 pb-4 items-center">
 		<div className="flex-1">
 			<Image
 				src="/media/artwork/art-001.jpg"
@@ -30,7 +30,7 @@ export default function Home() {
 			/>
 		</div>
 	  </div>
-	  <div className="flex gap-4 pb-4">
+	  <div className="flex gap-4 pb-4 items-center">
 		<div className="flex-1">
 		  <Image
 			src="/media/artwork/art-003.jpg"
@@ -48,7 +48,7 @@ export default function Home() {
 			/>
 		</div>
 	  </div>
-	  <div className="flex gap-4 pb-4">
+	  <div className="flex gap-4 pb-4 items-center">
 		<div className="flex-1">
 		  <Image
 			src="/media/artwork/art-005.jpg"
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,33 @@
 import Link from "next/link";
+import Image from "next/image";
 import Navbar from "@/components/Navbar";
 
 const Header = () => {
   return (
-    <header className="flex justify-between">
-        <Navbar />
+    <header >
+        <div className="flex justify-between"><Navbar /></div>
+        <div className="flex flex-col items-center pt-12">
+          <div className="w-full">
+    <Image 
+        src="/media/images/logo-16-9.png" 
+        alt="LM-Art" 
+        layout="responsive" 
+        width={1200} 
+        height={675}
+    />
+</div>
+<div className="w-full">
+    <Image 
+        src="/media/images/banner_1.jpg" 
+        alt="LM-Art" 
+        layout="responsive" 
+        width={1200} 
+        height={675}
+    />
+</div>
+          <p className="py-5 text-sm text-center">Art is the cure for the soul.</p>
+      </div>
+        <div className="pt-16 m:pt-24 pb-6"></div>
     </header>
   );
 };
